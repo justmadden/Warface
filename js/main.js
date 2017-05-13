adfddeebb0744d65f91f678c6436f0348b5d73fe
Denis Bura <justmadden@icloud.com>
2017-05-13 20:51:26 +0300

    Add
    Add

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+ jQuery(document).ready(function () {
+	//slick
+ jQuery('#slider').slick({
+		slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
+});
+
+});
